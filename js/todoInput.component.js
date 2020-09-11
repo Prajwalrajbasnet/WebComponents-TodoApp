8408cd2b7e82baf002e0f23df4b458e13d5d8a63
Prajwal Raj Basnet <basnetprajwalraj@gmail.com>
2020-09-11 14:16:04 +0545

    Refactor templates

--- js/todoInput.component.js
+++ js/todoInput.component.js
@@ -4,35 +4,7 @@ class TodoInput extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
-    this.formTemplate = html` <style>
-        @import url('https://fonts.googleapis.com/css2?family=Syne&display=swap');
-        .task-form {
-          width: 100%;
-        }
-        .task-field {
-          width: calc(100% - 24px);
-          border: none;
-          outline: none;
-          padding: 12px;
-          font-size: 18px;
-          background-color: #f1faee;
-          font-family: 'Syne', sans-serif;
-        }
-      </style>
-      <form
-        class="task-form"
-        @submit=${(ev) => {
-          this.handleSubmit(ev);
-        }}
-      >
-        <input
-          class="task-field"
-          type="text"
-          placeholder="Type the task you want to add and press enter"
-        />
-      </form>`;
-
-    render(this.formTemplate, this.shadowRoot);
+    this._render();
   }
 
   handleSubmit(ev) {
@@ -43,6 +15,39 @@ class TodoInput extends HTMLElement {
       new CustomEvent('submit', { detail: this.taskField.value })
     );
     this.taskField.value = '';
+  }
+
+  _render() {
+    render(
+      html` <style>
+          @import url('https://fonts.googleapis.com/css2?family=Syne&display=swap');
+          .task-form {
+            width: 100%;
+          }
+          .task-field {
+            width: calc(100% - 24px);
+            border: none;
+            outline: none;
+            padding: 12px;
+            font-size: 18px;
+            background-color: #f1faee;
+            font-family: 'Syne', sans-serif;
+          }
+        </style>
+        <form
+          class="task-form"
+          @submit=${(ev) => {
+            this.handleSubmit(ev);
+          }}
+        >
+          <input
+            class="task-field"
+            type="text"
+            placeholder="Type the task you want to add and press enter"
+          />
+        </form>`,
+      this.shadowRoot
+    );
   }
 }
 
--- js/todoItem.component.js
+++ js/todoItem.component.js
@@ -6,111 +6,133 @@ class TodoItem extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
+    this.deleteBtnTemplate = this.getDeleteBtnTemplate();
+  }
 
-    this.taskTogglerTemplate = (completed) =>
-      html`
-        <style>
-          .checkbox-label {
-            display: block;
-            position: relative;
-            cursor: pointer;
-            font-size: 22px;
-            line-height: 24px;
-            width: 24px;
-            clear: both;
-          }
+  connectedCallback() {
+    this._render();
+  }
 
-          .checkbox-label input {
-            position: absolute;
-            opacity: 0;
-            cursor: pointer;
-          }
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'task') {
+      this.task = newValue;
+    } else if (name === 'index') {
+      this.index = newValue;
+    } else if (name === 'completed') {
+      if (newValue === 'true') {
+        this.completed = true;
+      } else if ((newValue = 'false')) {
+        this.completed = false;
+      }
+    }
+    this._render();
+  }
 
-          .checkbox-label .checkbox-custom {
-            position: absolute;
-            top: 0px;
-            left: 0px;
-            height: 24px;
-            width: 24px;
-            background-color: transparent;
-            border-radius: 5px;
-            transition: all 0.3s ease-out;
-            -webkit-transition: all 0.3s ease-out;
-            -moz-transition: all 0.3s ease-out;
-            -ms-transition: all 0.3s ease-out;
-            -o-transition: all 0.3s ease-out;
-            border: 2px solid #555d50;
-          }
+  taskTogglerTemplate = (completed) =>
+    html`
+      <style>
+        .checkbox-label {
+          display: block;
+          position: relative;
+          cursor: pointer;
+          font-size: 22px;
+          line-height: 24px;
+          width: 24px;
+          clear: both;
+        }
 
-          .checkbox-label input:checked ~ .checkbox-custom {
-            background-color: #e63946;
-            border-radius: 5px;
-            -webkit-transform: rotate(0deg) scale(1);
-            -ms-transform: rotate(0deg) scale(1);
-            transform: rotate(0deg) scale(1);
-            opacity: 1;
-            border: 2px solid #e63946;
-          }
+        .checkbox-label input {
+          position: absolute;
+          opacity: 0;
+          cursor: pointer;
+        }
 
-          .checkbox-label .checkbox-custom::after {
-            position: absolute;
-            content: '';
-            left: 12px;
-            top: 12px;
-            height: 0px;
-            width: 0px;
-            border-radius: 5px;
-            border: solid #009bff;
-            border-width: 0 3px 3px 0;
-            -webkit-transform: rotate(0deg) scale(0);
-            -ms-transform: rotate(0deg) scale(0);
-            transform: rotate(0deg) scale(0);
-            opacity: 1;
-            transition: all 0.3s ease-out;
-            -webkit-transition: all 0.3s ease-out;
-            -moz-transition: all 0.3s ease-out;
-            -ms-transition: all 0.3s ease-out;
-            -o-transition: all 0.3s ease-out;
-          }
+        .checkbox-label .checkbox-custom {
+          position: absolute;
+          top: 0px;
+          left: 0px;
+          height: 24px;
+          width: 24px;
+          background-color: transparent;
+          border-radius: 5px;
+          transition: all 0.3s ease-out;
+          -webkit-transition: all 0.3s ease-out;
+          -moz-transition: all 0.3s ease-out;
+          -ms-transition: all 0.3s ease-out;
+          -o-transition: all 0.3s ease-out;
+          border: 2px solid #555d50;
+        }
 
-          .checkbox-label input:checked ~ .checkbox-custom::after {
-            -webkit-transform: rotate(45deg) scale(1);
-            -ms-transform: rotate(45deg) scale(1);
-            transform: rotate(45deg) scale(1);
-            opacity: 1;
-            left: 8px;
-            top: 3px;
-            width: 6px;
-            height: 12px;
-            border: solid #fff;
-            border-width: 0 2px 2px 0;
-            background-color: transparent;
-            border-radius: 0;
-          }
-        </style>
-        <label class="checkbox-label">
-          <input
-            class="toggle-task checkbox "
-            ?checked=${completed}
-            @click=${(e) => {
-              this.dispatchToggle(e);
-            }}
-            type="checkbox"
-          />
-          <span class="checkbox-custom"></span>
-        </label>
-      `;
+        .checkbox-label input:checked ~ .checkbox-custom {
+          background-color: #e63946;
+          border-radius: 5px;
+          -webkit-transform: rotate(0deg) scale(1);
+          -ms-transform: rotate(0deg) scale(1);
+          transform: rotate(0deg) scale(1);
+          opacity: 1;
+          border: 2px solid #e63946;
+        }
 
-    this.taskTitleTemplate = (task) => html` <style>
-        .taskTitle {
-          display: inline-block;
-          width: 70%;
-          padding-left: 55px;
+        .checkbox-label .checkbox-custom::after {
+          position: absolute;
+          content: '';
+          left: 12px;
+          top: 12px;
+          height: 0px;
+          width: 0px;
+          border-radius: 5px;
+          border: solid #009bff;
+          border-width: 0 3px 3px 0;
+          -webkit-transform: rotate(0deg) scale(0);
+          -ms-transform: rotate(0deg) scale(0);
+          transform: rotate(0deg) scale(0);
+          opacity: 1;
+          transition: all 0.3s ease-out;
+          -webkit-transition: all 0.3s ease-out;
+          -moz-transition: all 0.3s ease-out;
+          -ms-transition: all 0.3s ease-out;
+          -o-transition: all 0.3s ease-out;
+        }
+
+        .checkbox-label input:checked ~ .checkbox-custom::after {
+          -webkit-transform: rotate(45deg) scale(1);
+          -ms-transform: rotate(45deg) scale(1);
+          transform: rotate(45deg) scale(1);
+          opacity: 1;
+          left: 8px;
+          top: 3px;
+          width: 6px;
+          height: 12px;
+          border: solid #fff;
+          border-width: 0 2px 2px 0;
+          background-color: transparent;
+          border-radius: 0;
         }
       </style>
-      <span class="taskTitle">${task}</span>`;
+      <label class="checkbox-label">
+        <input
+          class="toggle-task checkbox "
+          ?checked=${completed}
+          @click=${(e) => {
+            this.dispatchToggle(e);
+          }}
+          type="checkbox"
+        />
+        <span class="checkbox-custom"></span>
+      </label>
+    `;
 
-    this.deleteBtnTemplate = html`
+  taskTitleTemplate = (task) => html` <style>
+      .taskTitle {
+        display: inline-block;
+        width: 70%;
+        padding-left: 55px;
+      }
+    </style>
+    <span class="taskTitle">${task}</span>`;
+
+  getDeleteBtnTemplate() {
+    return html`
       <style>
         button {
           background: none;
@@ -142,27 +164,6 @@ class TodoItem extends HTMLElement {
         X
       </button>
     `;
-  }
-
-  connectedCallback() {
-    this._render();
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    console.log(name, ' : ', newValue);
-
-    if (name === 'task') {
-      this.task = newValue;
-    } else if (name === 'index') {
-      this.index = newValue;
-    } else if (name === 'completed') {
-      if (newValue === 'true') {
-        this.completed = true;
-      } else if ((newValue = 'false')) {
-        this.completed = false;
-      }
-    }
-    this._render();
   }
 
   dispatchToggle(ev) {
